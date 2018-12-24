05c0576ae220c610c9d0e3b3cf8d08ca63b8bd0f
Jodie-Houghton <jodiehoughto@gmail.com>
2018-12-23 23:20:31 -0600

    Updates following Code Review suggestions

--- src/BooksList.js
+++ src/BooksList.js
@@ -1,45 +1,41 @@
-import React, { Component } from 'react'
+import React from 'react'
 import Bookshelf from './Bookshelf'
 import { Link } from 'react-router-dom'
 
-class BooksList extends Component {
-
-  render() {
-    const { books, bookChangeShelf } = this.props
-
-    return (
-      <div className="list-books">
-        <div className="list-books-title">
-          <h1>MyReads</h1>
-        </div>
-        <div className="list-books-content">
-          <div>
-            <Bookshelf
-              title='Currently Reading'
-              shelf='currentlyReading'
-              books={books}
-              bookChangeShelf={bookChangeShelf}
-              />
-            <Bookshelf
-              title='Want To Read'
-              shelf='wantToRead'
-              books={books}
-              bookChangeShelf={bookChangeShelf}
-              />
-            <Bookshelf
-              title='Read'
-              shelf='read'
-              books={books}
-              bookChangeShelf={bookChangeShelf}
-              />
-          </div>
-        </div>
-        <div className="open-search">
-          <Link to="/search">Add a book</Link>
+const BooksList = (props) => {
+  const { books, bookChangeShelf } = props
+  return (
+    <div className="list-books">
+      <div className="list-books-title">
+        <h1>MyReads</h1>
+      </div>
+      <div className="list-books-content">
+        <div>
+          <Bookshelf
+          title='Currently Reading'
+          shelf='currentlyReading'
+          books={books}
+          bookChangeShelf={bookChangeShelf}
+          />
+          <Bookshelf
+          title='Want To Read'
+          shelf='wantToRead'
+          books={books}
+          bookChangeShelf={bookChangeShelf}
+          />
+          <Bookshelf
+          title='Read'
+          shelf='read'
+          books={books}
+          bookChangeShelf={bookChangeShelf}
+          />
         </div>
       </div>
-    )
-  }
+    <div className="open-search">
+      <Link to="/search">Add a book</Link>
+    </div>
+  </div>
+  )
 }
 
 export default BooksList
--- src/Bookshelf.js
+++ src/Bookshelf.js
@@ -1,24 +1,21 @@
-import React, { Component } from 'react'
+import React from 'react'
 import Book from './Book'
 
-class Bookshelf extends Component {
-
-  render() {
-    const { title, shelf, books, bookChangeShelf } = this.props
-    return (
-        <div className="bookshelf">
-          <h2 className="bookshelf-title">{title}</h2>
-          <div className="bookshelf-books">
-            <ol className="books-grid">
-              {books.filter(b=>b.shelf===shelf).map(book =>
-                  <Book key={book.id} book={book}
-                    bookChangeShelf={bookChangeShelf}/>
-              )}
-            </ol>
-          </div>
-        </div>
-    )
-  }
+const Bookshelf = (props) => {
+  const { title, shelf, books, bookChangeShelf } = props
+  return (
+    <div className="bookshelf">
+      <h2 className="bookshelf-title">{title}</h2>
+      <div className="bookshelf-books">
+        <ol className="books-grid">
+          {books.filter(b=>b.shelf===shelf).map(book =>
+              <Book key={book.id} book={book}
+                bookChangeShelf={bookChangeShelf}/>
+          )}
+        </ol>
+      </div>
+    </div>
+  )
 }
 
 export default Bookshelf
